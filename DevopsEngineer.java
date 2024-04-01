6383eb648fd8529b000ae223829963e640cb17b1
01fe22bca163 <01fe22bca163@kletech.ac.in>
2024-04-01 17:44:02 +0530

    program to print devops engineer details

---
+++ DevopsEngineer.java
@@ -0,0 +1,7 @@
+public class DevopsEngineer {
+    float bonus=10000;
+    public void displaySalary(float salary){
+        salary+=bonus;
+        System.out.println("Total Salary:"+salary);
+    }
+}
--- Employee.java
+++ Employee.java
@@ -1,13 +1,16 @@
-public class Employee{
-    String emp_name="Ritul";
-     String emp_id="01fe22bca126";
+public class Employee extends DevopsEngineer{
+    String emp_name="Swapna";
+     String emp_id="01fe22bca163";
     public void displaydetails(){
         System.out.println("Name:"+emp_name);
         System.out.println("Employee id:"+emp_id);
     }
     public static void main(String[] args) {
-       Employee e=new Employee();
+       
+        float salary=100000;
+        Employee e=new Employee();
         e.displaydetails();
-    } 
+        e.displaySalary(salary);
+    }
 
 }
